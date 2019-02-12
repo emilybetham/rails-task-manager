3eb48a9b897cc836f884b1f8cabc1dba690cd96d
Emily Betham <emily.betham@bath.edu>
2019-02-12 15:29:05 +0100

    not pretty, but functional!

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,19 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # READ
+  get    'tasks',          to: 'tasks#index'
+
+  # CREATE
+  get    'tasks/new',      to: 'tasks#new', as: :new
+  post   'tasks',          to: 'tasks#create'
+
+  # SHOW
+  get    'tasks/:id',      to: 'tasks#show',  as: :show
+
+  # UPDATE
+  get    'tasks/:id/edit', to: 'tasks#edit', as: :edit
+  patch  'tasks/:id',      to: 'tasks#update', as: :task
+
+  # DESTROY
+  delete 'tasks/:id',      to: 'tasks#destroy', as: :destroy
 end
